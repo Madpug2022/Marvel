385e699c705d9572c62b344bcc55ff4eb5fc3b32
Matias <matias.chiappa@hotmail.com>
2024-11-14 16:27:06 +0100

    Feat: Header creation

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,19 @@
-import './Header.scss'
+import "./Header.scss";
+import logo from "../../assets/images/marvel-logo.png";
+import { AiFillHeart } from "react-icons/ai";
 
 function Header() {
-    return (
-        <header className='header'>
-            <div className='header__logo'>
-
-            </div>
-        </header>
-    )
+  return (
+    <header className="header">
+      <div className="header__logo">
+        <img src={logo} alt="Marvel Logo" />
+      </div>
+      <section className="header__favorites">
+        <AiFillHeart className="header__favorites__icon" />
+        <span className="header__favorites__text">0</span>
+      </section>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
